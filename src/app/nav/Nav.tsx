d8c5cc8630cd38972c1796541dec8a2cc220543e
Cindy Wong <cindywongdev@gmail.com>
2023-05-19 13:47:54 -0400

    styled nav

--- src/app/nav/Nav.tsx
+++ src/app/nav/Nav.tsx
@@ -3,20 +3,23 @@ import Link from 'next/link';
 export default function Nav(){
     return (
         <header>
-                    <div className='flex items-center justify-between flex-wrap bg-white p-6'>
-                        <div>Give2Learn</div>
-                        <Link href='/' className='mr-6'>
-                            Home
-                        </Link>
-                        <Link href='/for-donors' className='mr-6'>
-                            For donors
-                        </Link>
-                        <Link href='/for-teachers' className='mr-6'>
-                            For teachers
-                        </Link>
-                        <Link href='/profile' className='mr-6'>
-                            For profile
-                        </Link>
+                    <div className='flex items-center justify-between flex-wrap bg-white py-12'>
+                        <div className="title text-4xl">Give2Learn</div>
+
+                        <div className='flex gap-12'>
+                            <Link href='/' className='mr-6'>
+                                Home
+                            </Link>
+                            <Link href='/for-donors' className='mr-6'>
+                                For donors
+                            </Link>
+                            <Link href='/for-teachers' className='mr-6'>
+                                For teachers
+                            </Link>
+                            <Link href='/profile' className='mr-6'>
+                                Profile
+                            </Link>
+                        </div>
                     </div>
         </header>
     )
