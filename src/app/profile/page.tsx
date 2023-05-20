4b5b80962ec243605a27045266c14454ca26c541
MARY SANNI <marysanni@MARYs-MacBook-Pro.local>
2023-05-19 19:21:43 -0500

    my requests 1

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,28 +6,32 @@ function page() {
 		<div>
 			<h2>My Requests</h2>
 			{/*  */}
-			<div className=' padding: px justify-between flex bg-slate-100'>
-				<h1>Request 1234</h1>
-					<Image src={image3} alt=''width='40'></Image>
-			</div>
-			{/*  */}
-			{/* 3 */}
-			<div className='flex'>
-				<h1 className='mr-8'>laptop</h1>
-				<ul>
-					<li>model</li>
-					<li>dimensions</li>
-					<li>quantity</li>
-				</ul>
-			</div>
-			{/*  */}
-				<h1>Processing</h1>
-			{/*  */}
-					<ul>
-						<li>Expiration date</li>
-						<li>oo/00/0000</li>
-						<li>60 Days</li>
-					</ul>
+				<div className='bg-slate-100'>
+					<div className=' padding: px justify-between flex'>
+						<h1>Request 1234</h1>
+							<Image src={image3} alt=''width='40'></Image>
+					</div>
+					{/*  */}
+					{/* 3 */}
+					<div className='flex justify-between'>
+					<div className='flex'>
+						<h1 className='mr-8'>laptop</h1>
+						<ul>
+							<li>model</li>
+							<li>dimensions</li>
+							<li>quantity</li>
+						</ul>
+					</div>
+					{/*  */}
+						<h1>Processing</h1>
+					{/*  */}
+							<ul>
+								<li>Expiration date</li>
+								<li>oo/00/0000</li>
+								<li>60 Days</li>
+							</ul>
+					</div>
+				</div>
 			{/*  */}
 			<br></br>
 			{/*  */}
