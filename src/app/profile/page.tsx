06aa68de0cf41d95f5ee1fbdb3ae316b947d3e42
MARY SANNI <marysanni@MARYs-MacBook-Pro.local>
2023-05-20 10:25:45 -0500

    my requests 1&2

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -36,9 +36,31 @@ function page() {
 			<br></br>
 			{/*  */}
 			<div className='bg-slate-100'>
-				Request 2
-
-			</div>
+					<div className=' padding: px justify-between flex'>
+						<h1>Request 1234</h1>
+							<Image src={image3} alt=''width='40'></Image>
+					</div>
+					{/*  */}
+					{/* 3 */}
+					<div className='flex justify-between'>
+					<div className='flex'>
+						<h1 className='mr-8'>laptop</h1>
+						<ul>
+							<li>model</li>
+							<li>dimensions</li>
+							<li>quantity</li>
+						</ul>
+					</div>
+					{/*  */}
+						<h1>Processing</h1>
+					{/*  */}
+							<ul>
+								<li>Expiration date</li>
+								<li>oo/00/0000</li>
+								<li>60 Days</li>
+							</ul>
+					</div>
+				</div>
 			{/*  */}
 			<button className='py-2 px-5 bg-blue-500 rounded-lg' type='button'>Request More</button>
 		</div>
