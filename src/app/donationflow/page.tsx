311c291b74a02cab3061dfee1a8ca254d8b2cd70
Mortez-usa <hatam.info@gmail.com>
2023-05-19 17:03:11 -0700

    form steps

--- src/app/donationflow/page.tsx
+++ src/app/donationflow/page.tsx
@@ -8,6 +8,8 @@ export default function page() {
 		}
 	}
 
+
+
 	return (
 		<div className='pl-20 pr-20'>
 			<p className='pl-0 p-10'>
@@ -15,9 +17,13 @@ export default function page() {
 			</p>
 
 			<h1 className='text-lg font-bold'>Device Donations</h1>
-			<form action='/send-data-here' method='post'>
-				<label htmlFor='persional information'>
-					<p className='font-bold'>Personal information</p>
+			<form className='bg-blue-200 py-8 px-8' action='#' method='post'>
+				<div>
+					<h3 className='font-bold text-xl'>Device donation form</h3>
+					<p>1 of 3 steps</p>
+					<label className='font-bold' htmlFor='personal information'>
+						Personal information
+					</label>
 					<div className='flex gap-8'>
 						<input
 							className='bg-gray-100'
@@ -44,29 +50,156 @@ export default function page() {
 							required
 						/>
 					</div>
-				</label>
+				</div>
 				<label htmlFor='location'>
 					<p className='font-bold'>Location</p>
 					<p>where are you donating from?</p>
+				</label>
+				<div className='flex gap-8'>
+					<input
+						className='bg-gray-100'
+						placeholder='State'
+						type='text'
+					/>
+					<input
+						className='bg-gray-100'
+						placeholder='City'
+						type='text'
+					/>
+					<input
+						className='bg-gray-100'
+						placeholder='Street Address'
+						type='text'
+					/>
+				</div>
+				<button
+					title='button'
+					type='submit'
+					className='btn btn-primary py-5 px-1'>
+					<Link
+						className='bg-gray-400 hover:bg-gray-700 text-black text-xl py-1 px-4 rounded'
+						href='#'>
+						Next
+					</Link>
+				</button>
+				************************************
+				<div>
+					<h3 className='font-bold text-xl'>Device donation form</h3>
+					<p>2 of 3 steps</p>
+					<label className='font-bold' htmlFor='personal information'>
+						Device info
+					</label>
+					<p>what type of devices will you be donating?</p>
+					<div className='flex gap-8'>
+						<select
+							title='first-dropdown'
+							id='first-dropdown'
+							onchange='updateSecondDropdown()'>
+								
+							<option value='option1'>Option 1</option>
+							<option value='option2'>Option 2</option>
+							<option value='option3'>Option 3</option>
+						</select>
+						
+						<select title='second-dropdown' id='second-dropdown'>
+							<option value='default'>Choose an option</option>
+						</select>
+					</div>
+				</div>
+				<button
+					title='button'
+					type='submit'
+					className='btn btn-primary py-5 px-1'>
+					<Link
+						className='bg-gray-400 hover:bg-gray-700 text-black text-xl py-1 px-4 rounded'
+						href='#'>
+						Previous
+					</Link>
+				</button>
+				<button
+					title='button'
+					type='submit'
+					className='btn btn-primary py-5 px-1'>
+					<Link
+						className='bg-gray-400 hover:bg-gray-700 text-black text-xl py-1 px-4 rounded'
+						href='#'>
+						Next
+					</Link>
+				</button>
+				************************************
+				<div>
+					<label className='font-bold' htmlFor='personal information'>
+						Personal information
+					</label>
+					<p>3 of 3 steps</p>
 					<div className='flex gap-8'>
 						<input
 							className='bg-gray-100'
-							placeholder='State'
 							type='text'
+							id='fullname'
+							name='fullname'
+							placeholder='Fullname'
+							required
 						/>
 						<input
 							className='bg-gray-100'
-							placeholder='City'
 							type='text'
+							id='email'
+							name='email'
+							placeholder='Email'
+							required
 						/>
 						<input
 							className='bg-gray-100'
-							placeholder='Street Address'
 							type='text'
+							id='phone'
+							name='phone'
+							placeholder='Phone'
+							required
 						/>
 					</div>
+				</div>
+				<label htmlFor='location'>
+					<p className='font-bold'>Location</p>
+					<p>where are you donating from?</p>
 				</label>
-				<button title='button' type='submit' className='btn btn-primary'></button>
+				<div className='flex gap-8'>
+					<input
+						className='bg-gray-100'
+						placeholder='State'
+						type='text'
+					/>
+					<input
+						className='bg-gray-100'
+						placeholder='City'
+						type='text'
+					/>
+					<input
+						className='bg-gray-100'
+						placeholder='Street Address'
+						type='text'
+					/>
+				</div>
+				<button
+					title='button'
+					type='submit'
+					className='btn btn-primary py-5 px-1'>
+					<Link
+						className='bg-gray-400 hover:bg-gray-700 text-black text-xl py-1 px-4 rounded'
+						href='#'>
+						Previous
+					</Link>
+				</button>
+				<button
+					title='button'
+					type='submit'
+					className='btn btn-primary py-5 px-1'>
+					<Link
+						className='bg-gray-400 hover:bg-gray-700 text-black text-xl py-1 px-4 rounded'
+						href='#'>
+						Next
+					</Link>
+				</button>
 			</form>
 		</div>
 	);
