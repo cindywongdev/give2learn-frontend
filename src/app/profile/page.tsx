d09cbb9245b457bc7ef13ba27bf1fdd56c9150b8
MARY SANNI <marysanni@MARYs-MacBook-Pro.local>
2023-05-21 14:30:01 -0500

    update on requests

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,97 +1,133 @@
+'use client'
 import React from 'react';
 import Image from 'next/image';
-import image3 from '../image/arrow.png';
-import image4 from '../image/arrowup.png';
-import image5 from '../image/progress.png';
-import image6 from '../image/plus.png';
+import image1 from '../image/UI-icons/fill-in-2.png';
+import image2 from '../image/UI-icons/trash.png';
+import image3 from '../image/UI-icons/Laptop mac.png';
+import image4 from '../image/UI-icons/plus.png';
+import image5 from '../image/dl.png';
+import image6 from '../image/edit.png';
+import Link from 'next/link';
+import Nav from '../nav/Nav.tsx';
 function page() {
 	return (
-		<div className='p-5'>
-			<h2 className='mb-5 text-2xl/20 font-black text-xl'>My Requests</h2>
-			{/*  */}
-				<div className='bg-blue-200 p-10'>
-					<div className=' padding: px justify-between flex'>
-						<h1 className='font-bold mb-10'>Request 1234</h1>
-							<div >
-							<Image className='mb-10' src={image4} alt=''width='50'></Image>
-							</div>
-					</div>
-									<div className='mb-7 justify-between flex'>
-											<div>
-											<div><Image className='mb-10' src={image5} alt=''width='800'></Image></div>
-											<ul className='flex justify-between'>
-												<li className='w-20 '>Application under review</li>
-												<li>Approved</li>
-												<li>Out for shipping</li>
-												<li>Delivered</li>
-											</ul>
-											</div>
-											<div>
-												<ul>
-												<li>Expectation date</li>
-												<li>N/A</li>
-												</ul>
-											</div>
-									</div>
 
-					{/*  */}
-					{/* 3 */}
-					<div className='flex justify-between'>
-					<div className='flex'>
-						<h1 className=
-						'px-5 py-8 py-4 bg-white mr-8 font-medium'
-						>laptop</h1>
-						<ul>
-							<li>model</li>
-							<li>dimensions</li>
-							<li>quantity</li>
-						</ul>
-					</div>
-					<div> 
-						<p>7 days editing deadline disclamer</p>
-					<button className='  mt-7 py-2 px-5 bg-blue-500 rounded-lg mr-3 ' type='button'>Edit</button>
-					<button className='  mt-7 py-2 px-5 bg-blue-500 rounded-lg' type='button'>Cancel Request</button>
-					</div>
-
-					</div>
-				</div>
-			{/*  */}
-			<br></br>
-			{/*  */}
-			<div className='bg-blue-200 p-10'>
-					<div className=' padding: px justify-between flex'>
-						<h1 className='font-bold mb-10'>Request 1234</h1>
-							<Image className='mb-10' src={image3} alt=''width='30'></Image>
-					</div>
-					{/*  */}
-					{/* 3 */}
-					<div className='flex justify-between'>
-					<div className='flex'>
-						<h1 className=
-						'px-5 py-8 py-4 bg-white mr-8 font-medium'
-						>laptop</h1>
-						<ul>
-							<li>model</li>
-							<li>dimensions</li>
-							<li>quantity</li>
-						</ul>
-					</div>
-					{/*  */}
-						<h1>Processing</h1>
-					{/*  */}
-							<ul>
-								<li>Expiration date</li>
-								<li>oo/00/0000</li>
-								<li>60 Days</li>
-							</ul>
-					</div>
-				</div>
-			{/*  */}
-			
-			<div className='flex justify-center'>
-			<button className='  mt-7 py-2 px-5 bg-blue-500 rounded-lg' type='button'>Request More</button>
-			</div>
+		<div className='p-5 bg-g2l-bright-yellow'>
+            
+		{/*  */}
+		<div>
+			<Nav/>
 		</div>
+		<div className='mb-5' ><Link href='/'>&lt; Back to homepage</Link></div>
+		<h2 className='mb-5 text-2xl/20 font-black text-xl'>My Application</h2>
+{/* top container div */}  <div className='bg-blue-200 p-10'>
+						<div className='flex space-beween w-300' >
+			
+							<div>
+							<div className='progressbar flex items-center w-80 h-1 mr-80 mb-5 mt-5 justify-between bg-gray-200 rounded-full z-0'>
+									<div className='bg-blue-800 text-white progressbar-step flex w-8 h-8 rounded-full counter-reset counter-step items-center justify-center'>
+									1   
+									</div>
+									<div className='progressbar-step flex bg-gray-200 progressbar-step w-8 h-8 rounded-full counter-reset counter-step items-center justify-center'>
+									2
+									</div>
+									<div className='progressbar-step flex bg-gray-200 progressbar-step w-8 h-8 rounded-full counter-reset counter-step items-center justify-center'>
+									3
+									</div>
+									<div className='progressbar-step flex bg-gray-200 progressbar-step w-8 h-8 rounded-full counter-reset counter-step items-center justify-center'>
+									4
+									</div>
+						</div>
+
+									<ul className='flex '>
+											<li className='w-20 '>Application under review</li>
+											<li>Approved &nbsp; &nbsp;</li>
+											<li>Out for shipping &nbsp; &nbsp;</li>
+											<li>Delivered</li>
+									</ul>
+							</div>
+						<Image className='mb-10' src={image1} alt=''width='20'></Image>
+						<Image className='mb-10' src={image2} alt=''width='50'></Image>
+						
+						</div>
+				
+
+				
+						<br></br><br></br><br></br>
+				{/*  */}
+								<div className='justify-end flex'>
+									<Image className='mb-10 mr-2' src={image6} alt=''width='20'></Image>
+									<Image className='mb-10 ml-2' src={image5} alt=''width='20'></Image>
+								</div>
+				<div className='justify-between flex'>
+								<div className='justify-between flex'>
+									<div>
+									<Image className='mr-8' src={image3} alt=''width='90'></Image>
+									</div>
+									<ul>
+										<li>#0784329</li>
+										<li>Laptop</li>
+										<li>Dell, Dell Latitude 5000</li>
+									</ul>
+								</div>
+								{/*  */}
+								<div>
+									<h1>Application under review</h1>
+								</div>
+								{/*  */}
+							  <ul>
+										<li>Date: 05/22/23</li>
+										<li>Quantity:6</li>
+								</ul>
+						</div>
+				{/* 3 */}
+								<div className='justify-end flex'>
+									<Image className='mb-10 mr-2' src={image6} alt=''width='20'></Image>
+									<Image className='mb-10 ml-2' src={image5} alt=''width='20'></Image>
+								</div>
+				<div className='flex justify-between'>
+				<div className='flex'>
+					
+				</div>
+				<div> 
+					
+				</div>
+
+				</div>
+			</div>
+		{/*  */}
+		<br></br>
+		{/*  */}
+		
+							<div className=' bg-blue-200 p-10 justify-between flex'>
+								<div className='justify-between flex'>
+									
+									<div>
+									<Image className='mr-8' src={image3} alt=''width='90'></Image>
+									</div>
+									<ul>
+										<li>#0784329</li>
+										<li>Laptop</li>
+										<li>Dell, Dell Latitude 5000</li>
+									</ul>
+								</div>
+								{/*  */}
+								<div>
+									<h1>Application under review</h1>
+								</div>
+								{/*  */}
+							  <ul>
+										<li>Date: 05/22/23</li>
+										<li>Quantity:6</li>
+								</ul>
+						</div>
+		{/*  */}
+		
+		<div className='flex justify-center'>
+		<button className='  mt-7 py-2 px-5 bg-blue-500 rounded-lg' type='button'>Request More</button>
+		</div>
+	</div>
+
 		
 	);
 }
